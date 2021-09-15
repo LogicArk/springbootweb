cbc1f1e03f32dcbcee068dc4a9429deefdda6a48
LogicArk <1373569185@qq.com>
2021-09-15 11:56:39 +0800

    调整项目结构

---
+++ spring-boot-starter-demo/src/main/java/net/biancheng/springbootstarterdemo/SpringBootStarterDemoApplication.java
@@ -0,0 +1,13 @@
+package net.biancheng.springbootstarterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterDemoApplication.class, args);
+    }
+
+}
---
+++ spring-boot-starter-demo/src/test/java/net/biancheng/springbootstarterdemo/SpringBootStarterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.biancheng.springbootstarterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
