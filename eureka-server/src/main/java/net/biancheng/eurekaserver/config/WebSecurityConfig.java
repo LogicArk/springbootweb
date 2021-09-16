8148d40c6126ae0fd1451ced9ee83f88568807ed
LogicArk <1373569185@qq.com>
2021-09-16 15:51:04 +0800

    Eureka注册中心开启密码认证

---
+++ eureka-server/src/main/java/net/biancheng/eurekaserver/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package net.biancheng.eurekaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // 关闭csrf
+        httpSecurity.csrf().disable();
+        // 支持httpBasic
+        httpSecurity.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
