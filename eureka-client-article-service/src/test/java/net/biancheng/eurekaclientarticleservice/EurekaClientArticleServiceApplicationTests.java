659a7bddc95de62e4460822832c88bd4a573d806
LogicArk <1373569185@qq.com>
2021-09-16 15:12:38 +0800

    使用Eureka编写服务消费者

---
+++ eureka-client-article-service/src/test/java/net/biancheng/eurekaclientarticleservice/EurekaClientArticleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.biancheng.eurekaclientarticleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientArticleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
