cc5cdeb7f9e23590b79a478286d6c46eeaa5ab2a
LogicArk <1373569185@qq.com>
2021-10-03 17:28:16 +0800

    Spring Cloud Feign的自定义配置及使用: 日志配置

--- eureka-client-article-service/src/main/java/net/biancheng/eurekaclientarticleservice/client/UserRemoteClient.java
+++ eureka-client-article-service/src/main/java/net/biancheng/eurekaclientarticleservice/client/UserRemoteClient.java
@@ -1,9 +1,10 @@
 package net.biancheng.eurekaclientarticleservice.client;
 
+import net.biancheng.eurekaclientarticleservice.config.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "eureka-client-user-service")
+@FeignClient(value = "eureka-client-user-service",configuration = FeignConfiguration.class)
 public interface UserRemoteClient {
 
     @GetMapping("/user/hello")
